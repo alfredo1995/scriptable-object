921b52a6e33b5368e91a51bcf0917625a90e0054
Alfredo Gomes Pereira <71193893+alfredo1995@users.noreply.github.com>
2023-10-11 01:58:47 -0300

    first commit

---
+++ Scripts/TextScriptableObject.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "TestScriptableObject ", menuName = "ScriptableObject/TestScriptableObject")]
+public class TextScriptableObject : ScriptableObject
+{
+    public string textString;
+}
+
